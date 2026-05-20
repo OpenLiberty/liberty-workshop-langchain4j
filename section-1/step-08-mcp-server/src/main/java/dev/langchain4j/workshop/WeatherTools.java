9c64b8c59dbbac67ab939ad41ac352e60a3b33e4
Martin Smithson <msmiths@uk.ibm.com>
2026-05-20 10:19:27 +0100

    Working version
    Signed-off-by: Martin Smithson <msmiths@uk.ibm.com>

---
+++ section-1/step-08-mcp-server/src/main/java/dev/langchain4j/workshop/WeatherTools.java
@@ -0,0 +1,67 @@
+package dev.langchain4j.workshop;
+
+import io.openliberty.mcp.annotations.Tool;
+import io.openliberty.mcp.annotations.ToolArg;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * MCP Tools for accessing Meteo API functionality.
+ */
+@ApplicationScoped
+public class WeatherTools {
+
+    private static final Logger logger = LoggerFactory.getLogger(WeatherTools.class);
+
+    @Inject
+    @RestClient
+    private WeatherClient weatherClient;
+
+    /**
+     * Query the weather for the specified latitude and longitude
+     * 
+     * @param lattitude
+     *          Latitude of the location
+     * @param longitude
+     *          Longitude of the location
+     * @return String
+     *          The response from the Meteo API
+     */
+    @Tool(
+        name = "search_weather",
+        title = "Search for weather.",
+        description = "Search for weather at the specified location. "
+    )
+    public String searchNews(
+        @ToolArg(
+            name = "latitude",
+            description = "Latitude of the location."
+        )
+        double latitude,
+        @ToolArg(
+            name = "longitude",
+            description = "Longitude of the location"
+        )
+        double longitude
+    ) {
+        
+        logger.info("Querying the weather for location {}, {}", latitude, longitude);
+        
+        try {
+            return weatherClient.getForecast(
+                latitude,
+                longitude,
+                16,
+                "temperature_2m,snowfall,rain,precipitation,precipitation_probability"
+            );
+        } catch (Exception e) {
+            logger.error("Error querying weather", e);
+            throw new RuntimeException("Failed to query weather: " + e.getMessage(), e);
+        }
+    }
+}
